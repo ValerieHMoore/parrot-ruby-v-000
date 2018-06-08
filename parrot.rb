a303483358438888688d0964cc3d56a86f204a53
Valerie Moore <periwinkle17@outlook.com>
2018-06-08 14:50:17 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot()
-  puts "Squawk!"
-  return ()
+def parrot(string = "Squawk!")
+  puts string
+  return string
 end
